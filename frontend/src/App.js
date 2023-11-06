54896184a6f12bf414460db6dd195fcd133018ea
mariah16265 <mariah16265@gmail.com>
2023-11-06 19:04:04 +0400

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,41 @@
+import data from './data';
+//JSX is a syntax extension for JavaScript that allows you to write HTML-like code in your JavaScript files.
 function App() {
   return (
     <div>
       <header>
         <a href="/">amazon</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map(
+            (
+              products //to embed JavaScript within JSX.
+            ) => (
+              <div className="product" key={products.slug}>
+                <a href={`/products/${products.slug}`}>
+                  <img
+                    src={products.image}
+                    width="679"
+                    height="379"
+                    alt={products.name}
+                  ></img>
+                </a>
+                <div className="product-info">
+                  <a href={`/products/${products.slug}`}>
+                    <p>{products.name}</p>
+                  </a>
+                  <p>
+                    <strong>AED {products.price}</strong>
+                  </p>
+                  <button> Add to cart</button>
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      </main>
     </div>
   );
 }
