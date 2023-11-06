24e8947d65046b951d4ff905f8c1151c07c99d39
mariah16265 <mariah16265@gmail.com>
2023-11-06 22:03:41 +0400

    h

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,44 @@
 import { Link, useParams } from 'react-router-dom';
 // import data from '../data';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import axios from 'axios';
+import logger from 'use-reducer-logger';
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      //keep prev states value, only change loading to true
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      //action.payload:contains products from backend
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  });
+
   //useEffect is an asynchronous function, responsible for fetching data from an API.
   useEffect(() => {
     //This is an asynchronous function called fetchData that contains the logic for making an API request.
     //It uses the axios library to send a GET request to the /api/products endpoint. The result of the request is stored in the result variable.
     const fetchData = async () => {
-      //The await keyword is used to wait for the response from the API before continuing execution. The response is stored in the result variable.
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        //The await keyword is used to wait for the response from the API before continuing execution. The response is stored in the result variable.
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+
+      //setProducts(result.data);
     };
     fetchData();
   }, []);
@@ -19,29 +46,35 @@ function HomeScreen() {
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {products.map(
-          (
-            products //to embed JavaScript within JSX.
-          ) => (
-            <div className="product" key={products.slug}>
-              <Link to={`/products/${products.slug}`}>
-                <img
-                  src={products.image}
-                  width="679"
-                  height="379"
-                  alt={products.name}
-                ></img>
-              </Link>
-              <div className="product-info">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map(
+            (
+              products //to embed JavaScript within JSX.
+            ) => (
+              <div className="product" key={products.slug}>
                 <Link to={`/products/${products.slug}`}>
-                  <p>{products.name}</p>
+                  <img
+                    src={products.image}
+                    width="679"
+                    height="379"
+                    alt={products.name}
+                  ></img>
                 </Link>
-                <p>
-                  <strong>AED {products.price}</strong>
-                </p>
-                <button> Add to cart</button>
+                <div className="product-info">
+                  <Link to={`/products/${products.slug}`}>
+                    <p>{products.name}</p>
+                  </Link>
+                  <p>
+                    <strong>AED {products.price}</strong>
+                  </p>
+                  <button> Add to cart</button>
+                </div>
               </div>
-            </div>
+            )
           )
         )}
       </div>
