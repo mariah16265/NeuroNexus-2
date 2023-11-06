9cc5f0aed5a37be102bff158bae534aec13f6e89
mariah16265 <mariah16265@gmail.com>
2023-11-06 20:27:04 +0400

    yes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,25 @@
-import { Link } from 'react-router-dom';
-import data from '../data';
-
+import { Link, useParams } from 'react-router-dom';
+// import data from '../data';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  //useEffect is an asynchronous function, responsible for fetching data from an API.
+  useEffect(() => {
+    //This is an asynchronous function called fetchData that contains the logic for making an API request.
+    //It uses the axios library to send a GET request to the /api/products endpoint. The result of the request is stored in the result variable.
+    const fetchData = async () => {
+      //The await keyword is used to wait for the response from the API before continuing execution. The response is stored in the result variable.
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map(
+        {products.map(
           (
             products //to embed JavaScript within JSX.
           ) => (
