8636d2bb783b97aaed1c961ac2dd8f616e7396c1
mariah16265 <mariah16265@gmail.com>
2023-11-08 21:42:31 +0400

    improvement

--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -2,9 +2,34 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { Link, useParams } from 'react-router-dom';
 import Rating from './Rating';
+import axios from 'axios';
+import { useContext } from 'react';
+import { Store } from '../Store';
 
 function Product(props) {
   const { products } = props;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const addToCartHandler = async (item) => {
+    //check if current product exists in the cart,
+    const existItem = cartItems.find((x) => x._id === products._id);
+    //if it does, increase quantity by one, else set it to 1
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Out Of Stock!');
+      return;
+    }
+    //dispatch action on react context
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
+  };
   return (
     <Card>
       <Link to={`/products/${products.slug}`}>
@@ -20,12 +45,17 @@ function Product(props) {
         <Link to={`/products/${products.slug}`}>
           <Card.Title>{products.name}</Card.Title>
         </Link>
-        <Rating
-          rating={products.rating}
-          numReviews={products.numReviews}
-        ></Rating>
+        <Rating rating={products.rating} numReviews={products.numReviews} />
         <Card.Text>AED {products.price}</Card.Text>
-        <Button>Add To Cart</Button>
+        {products.countInStock === 0 ? (
+          <Button variant="light" disabled>
+            Out Of Stock
+          </Button>
+        ) : (
+          <Button onClick={() => addToCartHandler(products)}>
+            Add To Cart
+          </Button>
+        )}
       </Card.Body>
     </Card>
   );
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -10,10 +10,11 @@ import {
   Row,
 } from 'react-bootstrap';
 import MessageBox from '../Components/MessageBox';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 export default function CartScreen() {
+  const navigate = useNavigate();
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const {
     cart: { cartItems },
@@ -34,6 +35,10 @@ export default function CartScreen() {
 
   const removeItemHandler = (item) => {
     ctxDispatch({ type: 'CART_REMOVE_ITEM', payload: item });
+  };
+  //in sign in screen, we check user authentication, if authentiacted proceed to shipping
+  const checkoutHandler = () => {
+    navigate('/signin?redirect=/shipping');
   };
   return (
     <div>
@@ -124,6 +129,7 @@ export default function CartScreen() {
                     <Button
                       type="button"
                       variant="primary"
+                      onClick={checkoutHandler}
                       disabled={cartItems.length === 0}
                     >
                       Proceed to Checkout
